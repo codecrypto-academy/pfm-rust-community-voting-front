8bda9b61e7f337becf7fb000a21c27ccff58cf53
Carlos Navajas <c.navajas4@gmail.com>
2025-06-16 11:07:21 +0200

    chore: gh-pages package to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/pfm-rust-community-voting-front/',
   plugins: [
     react(),
     mode === 'development' &&
